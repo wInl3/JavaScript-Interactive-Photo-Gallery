25c5b9744f77f434d86a91b763f257b10935863a
wInl3 <thanglmhe187393@fpt.edu.vn>
2024-10-19 17:09:29 +0700

    GalleryHomework.js

--- JS - Gallery Homework/GalleryHomework.js
+++ JS - Gallery Homework/GalleryHomework.js
@@ -1,24 +1,11 @@
-/*Name this external file gallery.js*/
-
-function upDate(previewPic) {
-    // Check if the function is triggered
-    console.log("upDate triggered");
-
-    // Log details about the previewPic
-    console.log("Image Source: ", previewPic.src);
-    console.log("Image Alt: ", previewPic.alt);
-
-    // Change the background image of the div with id = "image"
-    document.getElementById("image").style.backgroundImage = `url('${previewPic.src}')`;
-
-    // Change the text of the div with id = "image"
-    document.getElementById("image").innerHTML = previewPic.alt;
+function upDate(previewPic){
+    var src = previewPic.getAttribute( "src" );
+    document.getElementById('image').style.backgroundImage = "url('" + src + "')";
+    document.getElementById('image').innerHTML = previewPic.alt;
 }
 
-	function unDo() {
-    // Update the url for the background image of the div with the id = "image"
-    document.getElementById("image").style.backgroundImage = "url('')";
-
-    // Change the text of the div with id = "image" back to the original text
-    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+function unDo(){
+    var text = "Hover over an image below to display here.";
+    document.getElementById('image').style.backgroundImage = "url('')";
+    document.getElementById('image').innerHTML= text;
 }
